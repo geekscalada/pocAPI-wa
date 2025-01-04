97e7bae16a2f81d484b2c4297dc8bc00583f2de3
Jose <geekscalada@gmail.com>
2025-01-04 18:44:53 +0100

    change build to add node_modules

--- infra/lib/lambda-stack.ts
+++ infra/lib/lambda-stack.ts
@@ -9,19 +9,6 @@ export class LambdaStack extends Stack {
 
     const { projectName, environmentName } = props;
 
-    const lambdaBaseName = 'jep_HelloWorld_lambda';
-
-    const myLambda = new lambda.Function(
-      this,
-      `${projectName}-${environmentName}-${lambdaBaseName}`,
-      {
-        functionName: `${projectName}-${environmentName}-${lambdaBaseName}`,
-        runtime: lambda.Runtime.NODEJS_18_X,
-        code: lambda.Code.fromAsset('../lambdas/my-lambda/dist'),
-        handler: 'index.handler',
-      },
-    );
-
     const newLambdaBaseName = 'jep_new_lambda';
 
     const newLambda = new lambda.Function(
