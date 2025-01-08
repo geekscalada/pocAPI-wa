800012d1c2461f2c903253a28ee02df4c6ed2298
Jose <geekscalada@gmail.com>
2025-01-08 14:00:34 +0100

    fix: move index.ts

--- lambdas/my-lambda/src/index.ts
+++ lambdas/my-lambda/src/index.ts
@@ -1,6 +1,6 @@
 // Import always first
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
-import { IOservice } from './IOservice';
+import { IOservice } from './services/IOservice';
 
 export const handler = async (context: Context, event: APIGatewayEvent) => {
   const ioService = new IOservice();
