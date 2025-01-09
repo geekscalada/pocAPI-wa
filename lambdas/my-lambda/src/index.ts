cba239068bda32c48e5212732f3f2e843067e86b
Jose <geekscalada@gmail.com>
2025-01-09 13:32:27 +0100

    fix: update module resolution and import paths to use .js extensions as  ESM requirements

--- lambdas/my-lambda/src/index.ts
+++ lambdas/my-lambda/src/index.ts
@@ -1,6 +1,6 @@
 // Import always first
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
-import { IOservice } from './services/IOservice';
+import { IOservice } from './services/IOservice.js';
 
 export const handler = async (context: Context, event: APIGatewayEvent) => {
   const ioService = new IOservice();
