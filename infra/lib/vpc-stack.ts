9b25a669ee9ef40c52380ca42c441697e8a56130
Jose Escalada <geekscalada@gmail.com>
2025-12-08 17:07:45 +0100

    feat: implement second VPC, TGW attachment and Redis

--- infra/lib/vpc-stack.ts
+++ infra/lib/vpc-stack.ts
@@ -101,31 +101,8 @@ export class VpcStack extends Stack {
     attachmentVpc2.addDependency(this.transitGateway);
 
     // Routing entre VPC1 (10.0.0.0/16) y VPC2 (10.1.0.0/16) vía TGW
-    const tgwRouteTable = new ec2.CfnTransitGatewayRouteTable(this, 'TransitGatewayRouteTable', {
-      transitGatewayId: this.transitGateway.ref,
-    });
-
-    const associationVpc1 = new ec2.CfnTransitGatewayRouteTableAssociation(this, 'TgwRtAssocVpc1', {
-      transitGatewayAttachmentId: attachmentVpc1.ref,
-      transitGatewayRouteTableId: tgwRouteTable.ref,
-    });
-
-    const associationVpc2 = new ec2.CfnTransitGatewayRouteTableAssociation(this, 'TgwRtAssocVpc2', {
-      transitGatewayAttachmentId: attachmentVpc2.ref,
-      transitGatewayRouteTableId: tgwRouteTable.ref,
-    });
-
-    new ec2.CfnTransitGatewayRoute(this, 'RouteToVpc1', {
-      transitGatewayRouteTableId: tgwRouteTable.ref,
-      destinationCidrBlock: '10.0.0.0/16',
-      transitGatewayAttachmentId: attachmentVpc1.ref,
-    }).addDependency(associationVpc1);
-
-    new ec2.CfnTransitGatewayRoute(this, 'RouteToVpc2', {
-      transitGatewayRouteTableId: tgwRouteTable.ref,
-      destinationCidrBlock: '10.1.0.0/16',
-      transitGatewayAttachmentId: attachmentVpc2.ref,
-    }).addDependency(associationVpc2);
+    // Usamos la route table por defecto del TGW (defaultRouteTableAssociation/Propagation habilitados),
+    // añadiendo solo rutas explícitas en las tablas de ruta de las VPC.
 
     // Rutas en las tablas de ruta de las subnets privadas de ambas VPC
     privateSubnetsVpc1.forEach((subnet, index) => {
