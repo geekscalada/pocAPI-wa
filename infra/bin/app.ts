ce32b15150b6c7a386b2ee2b18e70a66179bee6b
Jose Escalada <geekscalada@gmail.com>
2025-12-16 23:39:57 +0100

    feat: test EC2

--- infra/bin/app.ts
+++ infra/bin/app.ts
@@ -45,8 +45,15 @@ const internalBucketStack = new InternalBucketStack(
 );
 
 // Stack para crear instancias de prueba en cada VPC (SSM-enabled)
-const testConnVpcStack = new TestConnVpcStack(app, 'TestConnVpcStack', { ...secretValues, vpc1: vpcStack.vpc1, vpc2: vpcStack.vpc2 });
-
+interface TestConnVpcProps extends InfraProps {
+  vpc1: import('aws-cdk-lib/aws-ec2').IVpc;
+  vpc2: import('aws-cdk-lib/aws-ec2').IVpc;
+}
+const testConnVpcStack = new TestConnVpcStack(app, 'TestConnVpcStack', { 
+  ...secretValues, 
+  vpc1: vpcStack.vpc1, 
+  vpc2: vpcStack.vpc2 
+} as TestConnVpcProps);
 testConnVpcStack.addDependency(vpcStack);
 
 // Testing API Stack (usa publisherLambda que ya tiene SNS configurado)
