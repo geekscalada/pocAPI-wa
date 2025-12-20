1afadcd89b880fcf50ae3d6c2221cb2778afc8b7
Jose Escalada <geekscalada@gmail.com>
2025-12-20 12:47:46 +0100

    feat: remove connVPCstack

--- infra/bin/app.ts
+++ infra/bin/app.ts
@@ -4,9 +4,9 @@ import { SnsTestStack } from '../lib/sns-test-stack.js';
 import { InternalBucketStack } from '../lib/internal-bucket-stack.js';
 import { LambdaStack } from '../lib/lambda-stack.js';
 import { SqsStack } from '../lib/sqs-stack.js';
-// import { TestingApiStack } from '../lib/testing-api-stack.js';
-// import { VpcStack } from '../lib/vpc-stack.js';
-import { TestConnVpcStack } from '../lib/test-conn-vpc-stack.js';
+import { TestingApiStack } from '../lib/testing-api-stack.js';
+ import { VpcStack } from '../lib/vpc-stack.js';
+// import { TestConnVpcStack } from '../lib/test-conn-vpc-stack.js';
 import { App, StackProps } from 'aws-cdk-lib';
 
 export interface InfraProps extends StackProps {
@@ -29,14 +29,14 @@ if (!secretValues) {
 }
 
 // Stacks
-// const vpcStack = new VpcStack(app, 'VpcStack', secretValues);
+const vpcStack = new VpcStack(app, 'VpcStack', secretValues);
 const snsTestStack = new SnsTestStack(app, "SnsTestStack", secretValues);
 const sqsStack = new SqsStack(app, "SqsStack", { ...secretValues, testTopic: snsTestStack.testTopic });
 const lambdaStack = new LambdaStack(app, `LambdaStack-prueba`, { 
   ...secretValues, 
   testTopic: snsTestStack.testTopic, 
-  // vpc1: vpcStack.vpc1,
-  // vpc2: vpcStack.vpc2,
+  vpc1: vpcStack.vpc1,
+  vpc2: vpcStack.vpc2,
 });
 const internalBucketStack = new InternalBucketStack(
   app,
@@ -57,23 +57,23 @@ const internalBucketStack = new InternalBucketStack(
 // // testConnVpcStack.addDependency(vpcStack);
 
 // Testing API Stack (usa publisherLambda que ya tiene SNS configurado)
-// const testingApiStack = new TestingApiStack(
-//   app, 
-//   "TestingApiStack", 
-//   secretValues, 
-//   lambdaStack.publisherLambda,
-//   sqsStack.directProducerLambda,
-//   sqsStack.dedupDirectProducerLambda,
-//   sqsStack.apiDirectQueue.queueName,
-//   sqsStack.apiDirectQueue.queueArn
-// );
+const testingApiStack = new TestingApiStack(
+  app, 
+  "TestingApiStack", 
+  secretValues, 
+  lambdaStack.publisherLambda,
+  sqsStack.directProducerLambda,
+  sqsStack.dedupDirectProducerLambda,
+  sqsStack.apiDirectQueue.queueName,
+  sqsStack.apiDirectQueue.queueArn
+);
 
 // Orden del despliegue
 sqsStack.addDependency(snsTestStack);
 lambdaStack.addDependency(snsTestStack);
-// lambdaStack.addDependency(vpcStack);
-// testingApiStack.addDependency(lambdaStack);
-// testingApiStack.addDependency(sqsStack);
+lambdaStack.addDependency(vpcStack);
+testingApiStack.addDependency(lambdaStack);
+testingApiStack.addDependency(sqsStack);
 
 /**
  * Permissions S3 to lambdas
